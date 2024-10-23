81c5ddc1f9e95afd0b7a4972e0257e8e0b3ed497
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-10-23 19:47:39 +0300

    1-st stage

---
+++ src/TicTacToe.java
@@ -0,0 +1,22 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        printBoard(board);
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+}
