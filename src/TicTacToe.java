f408fbf1df99200b0d6c6fca2cf56b9b037d62b3
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-10-23 19:49:49 +0300

    3-st stage

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,6 +15,7 @@ public class TicTacToe {
             }
         }
         printBoard(board);
+        analyzeGame(board);
     }
 
     public static void printBoard(char[][] board) {
@@ -27,5 +28,41 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("---------");
+    }
+
+    public static void analyzeGame(char[][] board) {
+        int xCount = 0;
+        int oCount = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 'X') xCount++;
+                if (board[i][j] == 'O') oCount++;
+            }
+        }
+
+        boolean xWins = checkWin(board, 'X');
+        boolean oWins = checkWin(board, 'O');
+
+        if (xWins && oWins || Math.abs(xCount - oCount) > 1) {
+            System.out.println("Impossible");
+        } else if (xWins) {
+            System.out.println("X wins");
+        } else if (oWins) {
+            System.out.println("O wins");
+        } else if (xCount + oCount == 9) {
+            System.out.println("Draw");
+        } else {
+            System.out.println("Game not finished");
+        }
+    }
+
+    public static boolean checkWin(char[][] board, char player) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true;
+            if (board[0][i] == player && board[1][i] == player && board[2][i] == player) return true;
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) return true;
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true;
+        return false;
     }
 }
