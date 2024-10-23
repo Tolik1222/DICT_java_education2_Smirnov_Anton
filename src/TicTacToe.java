1516c576e5779c62579d2a8524e3d6070bb71a92
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-10-23 19:47:52 +0300

    2-st stage

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        char[][] board = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
-        };
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        char[][] board = new char[3][3];
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = input.charAt(index);
+                index++;
+            }
+        }
         printBoard(board);
     }
 
