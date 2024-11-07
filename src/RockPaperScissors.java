89483c4e8aef7d21146cfd4f85619791307752c0
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-11-07 11:15:55 +0300

    3-st stage

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -7,24 +7,31 @@ public class RockPaperScissors {
         Random random = new Random();
         String[] choices = {"rock", "paper", "scissors"};
 
-        System.out.print("Enter rock, paper, or scissors: ");
-        String userChoice = scanner.next().toLowerCase();
+        while (true) {
+            System.out.print("Enter rock, paper, scissors, or !exit: ");
+            String userChoice = scanner.next().toLowerCase();
 
-        if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
-            System.out.println("Invalid input");
-            return;
-        }
+            if (userChoice.equals("!exit")) {
+                System.out.println("Bye!");
+                break;
+            }
 
-        String computerChoice = choices[random.nextInt(3)];
+            if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+                System.out.println("Invalid input");
+                continue;
+            }
 
-        if (userChoice.equals(computerChoice)) {
-            System.out.println("There is a draw (" + computerChoice + ")");
-        } else if ((userChoice.equals("rock") && computerChoice.equals("scissors")) ||
-                (userChoice.equals("paper") && computerChoice.equals("rock")) ||
-                (userChoice.equals("scissors") && computerChoice.equals("paper"))) {
-            System.out.println("Well done. The computer chose " + computerChoice + " and failed");
-        } else {
-            System.out.println("Sorry, but the computer chose " + computerChoice);
+            String computerChoice = choices[random.nextInt(3)];
+
+            if (userChoice.equals(computerChoice)) {
+                System.out.println("There is a draw (" + computerChoice + ")");
+            } else if ((userChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                    (userChoice.equals("paper") && computerChoice.equals("rock")) ||
+                    (userChoice.equals("scissors") && computerChoice.equals("paper"))) {
+                System.out.println("Well done. The computer chose " + computerChoice + " and failed");
+            } else {
+                System.out.println("Sorry, but the computer chose " + computerChoice);
+            }
         }
     }
 }
